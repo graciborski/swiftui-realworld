fb5f3dc729c316d1a4f5c9f9258f4f02ac3e9dda
Grzegorz Raciborski <raciborskigrzegorz@gmail.com>
2019-08-18 08:12:58 +0200

    Create iOS project

---
+++ Conduit/Conduit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Conduit
+//
+//  Created by Grzegorz Raciborski on 18/08/2019.
+//  Copyright © 2019 Grzegorz Raciborski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ kudos.md
@@ -0,0 +1,4 @@
+# Kudos
+
+- [PointFree](https://www.pointfree.co/)
+- [Modern modular apps with Xcode 11 and Swift Package Manager - Kane](https://edit.theappbusiness.com/modern-modular-apps-with-xcode-11-and-swift-package-manager-6b4afa0125be)
